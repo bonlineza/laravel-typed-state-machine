6a4dfec40014dc9e53daee6871f0c7f194f4b00e
Shaun van Tonder <shaunmichaelvantonder@gmail.com>
2019-10-31 15:08:25 +0200

    Override fireEvent to use the SerializesModels trait on events
    This overrides the fireEvent model to rather use the Laravel event
    system for the events of the StateMachine.

--- src/LaravelStateMachine.php
+++ src/LaravelStateMachine.php
@@ -2,27 +2,27 @@
 
 namespace TypedStateMachine\Laravel;
 
+use Bagf\Dynamic\Builder as DynamicBuilder;
+use Illuminate\Queue\SerializesModels;
+use TypedStateMachines\IEvent;
 use TypedStateMachines\StateMachine;
 
 class LaravelStateMachine extends StateMachine
 {
 	/**
-     * Fires the provided event into the bus.
+     * Overrides the fireEvent if StateMachine to use Laravel's event system.
+     * Shout out to @bagf for bagf/dynamic which made this neat and easy. 
+     * Give it a star!
      *
      * @param IEvent $event
      * @return void
      */
     public function fireEvent(IEvent $event)
     {
-        $class     = get_class($event);
-        $listeners = $this->getListeners();
+        $event = DynamicBuilder::fromInstance($event)
+            ->shareTrait(SerializesModels::class)
+            ->instance();
 
-        if (array_key_exists($class, $listeners)) {
-            foreach ($listeners[$class] as $listener) {
-                /** @var IListener $listenerInstance */
-                $listenerInstance = new $listener();
-                $listenerInstance->handle($event);
-            }
-        }
+        event($event);
     }
 }
